3a8afaf3616c717876eb3929024bc3e42db52a09
kavitalailia <127377587+kavitalailia@users.noreply.github.com>
2023-03-31 16:55:24 +0700

    Delete latihanprokom17maret.py

--- latihanprokom17maret.py
+++
@@ -1,7 +0,0 @@
-mata_uang = {
-    'IDR' : 1,
-    'USD' : 0.000071,
-}
-uang = input("masukkan mata uang yang ingin dikonversi (IDR, USD): ")
-nilai = int(input("masukan nilai uang: "))
-print(mata_uang[uang]*nilai) 
